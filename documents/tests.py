37f2afbd7339aaeb77a8d52414db9032ca0acac5
ZhenyaKasatkina <knopisha@list.ru>
2024-07-28 18:22:17 +0300

    change documents/tests.py, docker-compose.yml

--- documents/tests.py
+++ documents/tests.py
@@ -113,26 +113,3 @@ class EducationTestCase(APITestCase):
     def tearDown(self):
         translation.activate(settings.LANGUAGE_CODE)
         super().tearDown()
-
-
-class EmailTest(TestCase):
-
-    def test_send_email(self):
-        """Отправка сообщения"""
-
-        # Empty the test outbox
-        mail.outbox = []
-        # Send message.
-        send_email_message(
-            "Subject here",
-            "Here is the message.",
-            "from@example.com",
-        )
-        # Test that one message has been sent.
-        self.assertEqual(len(mail.outbox), 1)
-        # Verify that the subject of the first message is correct.
-        self.assertEqual(mail.outbox[0].subject, "Subject here")
-
-    def tearDown(self):
-        translation.activate(settings.LANGUAGE_CODE)
-        super().tearDown()
